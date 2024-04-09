d8d414b612cb24c5cbb483f8da16519170faac4e
imjowend <joaquinvcestudio@gmail.com>
2024-04-09 07:59:22 -0500

    scaffolding

--- frontend/example.py
+++ frontend/example.py
@@ -9,6 +9,9 @@ st.title('App de análisis de datos predictivo para calificación de riesgo')
 
 st.write("El objetivo de este proyecto es realizar un MVP que muestre un reporte de análisis de datos predictivo para establecer la calificación de riesgo de potenciales clientes, al momento de otorgar préstamos.")
 
-salary = st.number_input('Ingrese su salario:') 
+salary = st.number_input('¿Cual es su salario?') 
 
 st.write(f"Su salario es: ${salary}")
+
+study = st.radio("¿Cual es su nivel de estudio alcanzado?", ("Secundario completo", "Secundario incompleto", "Terceario completo", "Terceario incompleto", "Universidad incompleto"))
+
