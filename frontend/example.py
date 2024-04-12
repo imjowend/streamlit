268ca07a07ee13c57bdb481eee51a6fcf3d47e03
imjowend <joaquinvcestudio@gmail.com>
2024-04-12 11:08:59 -0500

    feat: input radio

--- frontend/example.py
+++ frontend/example.py
@@ -19,3 +19,13 @@ casa_depto_propio = st.radio("Cuenta con departamento propio?", ("Si", "No"))
 quien_acompano = st.radio("Quien la acompaño al momento de pedir el credito", ('Sin compañia', 'Familia', 'Conyugue o Pareja', 'Niños', 'Grupo de personas'))
 
 estatus_laboral = st.radio("Cual es su situacion laboral actual?", ('Trabajador', 'Funcionario', 'Socio comercial', 'Pensionista', 'Desempleado', 'Estudiante', 'Empresario', 'Baja por maternidad'))
+
+nivel_educacion = st.radio("Cual es su nivel de educacion?", ('Secundaria / secundaria especial', 'Educación superior', 'Superior incompleta', 'Secundaria inferior', 'Titulación académica'))
+
+estado_civil = st.radio("Cual es su estado civil?", ('Soltero / no casado', 'Casado', 'Matrimonio civil', 'Viudo', 'Separado'))
+
+forma_habitar = st.radio("En que tipo de residenciadonde habita?", ("Casa/apartamento", "Apartamento alquilado", "Con los padres", "Apartamento municipal", "Apartamento oficina", "Apartamento cooperativa"))
+
+ocupacion = st.radio("Que tipo de trabajo tiene?", ('Obreros' 'Personal de base' 'Otros' 'Directivos' 'Conductores' 'Personal de ventas'))
+
+dia_inicio_proceso = st.radio("Que dia de la semana pidió el credito?", ("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"))
