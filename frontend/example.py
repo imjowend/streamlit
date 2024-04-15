eb253ea4bd842141335ac05a954ed23f1f86658e
imjowend <joaquinvcestudio@gmail.com>
2024-04-14 23:54:05 -0500

    feat: inputs radios

--- frontend/example.py
+++ frontend/example.py
@@ -24,8 +24,26 @@ nivel_educacion = st.radio("Cual es su nivel de educacion?", ('Secundaria / secu
 
 estado_civil = st.radio("Cual es su estado civil?", ('Soltero / no casado', 'Casado', 'Matrimonio civil', 'Viudo', 'Separado'))
 
-forma_habitar = st.radio("En que tipo de residenciadonde habita?", ("Casa/apartamento", "Apartamento alquilado", "Con los padres", "Apartamento municipal", "Apartamento oficina", "Apartamento cooperativa"))
+forma_habitar = st.radio("En que tipo de residencia habita?", ("Casa/apartamento", "Apartamento alquilado", "Con los padres", "Apartamento municipal", "Apartamento oficina", "Apartamento cooperativa"))
 
 ocupacion = st.radio("Que tipo de trabajo tiene?", ('Obreros', 'Personal de base', 'Otros', 'Directivos', 'Conductores', 'Personal de ventas'))
 
 dia_inicio_proceso = st.radio("Que dia de la semana pidió el credito?", ("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"))
+
+tipo_organizacion_trabajo = st.radio("En que area trabaja",('Empresas', 'Educación', 'Administración', 'Otros', 'Construcción', 'Medicina', 'Autónomos', 'Transporte', 'Inmobiliario', 'Comercio', 'Industria', 'Fuerzas Armadas', 'Finanzas'))
+
+telefono_trabajo = st.radio("Entregó telefono de trabajo cuando postuló?", ("Si", "No"))
+
+telefono_casa = st.radio("Entregó telefono de casa cuando postuló?", ("Si", "No"))
+
+telefono_casa2 = st.radio("Entregó telefono de casa2 cuando postuló?", ("Si", "No"))
+
+reg_residencia_diferente = st.radio("Si la residencia indicada es distinta a la real", ("Si", "No") )
+
+reg_trabajo_diferente= st.radio("Si la direccion de trabajo indicada es distinta a la real", ("Si", "No") )
+
+city_residencia_diferente= st.radio("Si la ciudad de residencia indicada es distinta a la real", ("Si", "No") )
+
+city_trabajo_diferente= st.radio("Si la ciudad de trabajo indicada es distinta a la real", ("Si", "No") )
+
+live_trabajo_diferente= st.radio("Si la ciudad de trabajo indicada es distinta a donde vive", ("Si", "No") )
