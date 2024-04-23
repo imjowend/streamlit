d1b5c911d604dc94db424a4358649861960c92d4
imjowend <joaquinvcestudio@gmail.com>
2024-04-22 23:26:39 -0500

    html

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get('/')
 def message():
-    return "Hello world"
+    return HTMLResponse('<h1>Hello world<h1/>')
